bd0404e9b1766bc82f5e3325f139b1cea0551594
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-10 23:58:32 +0100

    add "Profile" and "Tweet" models

--- src/app_warbler/models.py
+++ src/app_warbler/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user         = models.OneToOneField(User, on_delete=models.CASCADE)
+    # photo        = models.
+    about_myself = models.TextField(max_length=256, null=True, blank=True)
+
+
+class Tweet(models.Model):
+    user              = models.ForeignKey(User, on_delete=models.CASCADE)
+    content           = models.TextField()
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    update_datetime   = models.DateTimeField(auto_now=True)
+
