f88da4632a54c2f0d28b7ea05d1eae76818cdcdb
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-10 23:58:01 +0100

    add STATIC_ROOT = os.path.join(BASE_DIR, 'static')

--- src/warbler/settings.py
+++ src/warbler/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'warbler.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'app_warbler/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
