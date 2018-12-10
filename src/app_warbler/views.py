eac77dae3f5003362479ff9d870f70a75f328d67
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-10 23:58:43 +0100

    add ListAllTweetsView

--- src/app_warbler/views.py
+++ src/app_warbler/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from . import models
 
-# Create your views here.
+
+class ListAllTweetsView(generic.ListView):
+    model = models.Tweet
+    template_name = 'app_warbler/all_tweets.html'
+    paginate_by = 10
