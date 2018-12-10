15699fab3862d17cd102c42df5edd755452ee0f9
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-11 00:08:15 +0100

    add ProfileForm and TweetForm

---
+++ src/app_warbler/forms.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.7
+
+from django import forms
+from . import models
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.Profile
+        fields = '__all__'
+
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = models.Tweet
+        fields = '__all__'
