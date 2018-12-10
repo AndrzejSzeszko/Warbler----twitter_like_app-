7c9d00ccb2dee4a4e1b841e200dab71de4dacfd0
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-10 23:57:36 +0100

    add path('', views.ListAllTweetsView.as_view(), name='list-all-tweets')

--- src/warbler/urls.py
+++ src/warbler/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_warbler import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.ListAllTweetsView.as_view(), name='list-all-tweets')
 ]
