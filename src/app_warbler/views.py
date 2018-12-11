098dd4078aa373cb06ad8762b358ebbf3c84e35c
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-11 23:54:39 +0100

    add ordering = ['-update_datetime'] in ListAllTweetsView. Add TweetDetailsView and CreateTweetView

--- src/app_warbler/views.py
+++ src/app_warbler/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
-from . import models
+from . import (
+    models,
+    forms
+)
 
 
 class ListAllTweetsView(generic.ListView):
-    model = models.Tweet
+    model         = models.Tweet
     template_name = 'app_warbler/all_tweets.html'
-    paginate_by = 10
+    paginate_by   = 10
+    ordering      = ['-update_datetime']
+
+
+class TweetDetailsView(generic.DetailView):
+    model         = models.Tweet
+    template_name = 'app_warbler/tweet_details.html'
+
+
+class CreateTweetView(LoginRequiredMixin, generic.CreateView):
+    model         = models.Tweet
+    template_name = 'app_warbler/create_tweet.html'
+    fields        = ['content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
