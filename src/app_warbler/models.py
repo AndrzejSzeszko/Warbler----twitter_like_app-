17dd28a9bcdc40f149cee9c8c5b08f0974c4785a
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-11 23:55:15 +0100

    add get_absolute_url fnc in Tweet model

--- src/app_warbler/models.py
+++ src/app_warbler/models.py
@@ -1,17 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db import models
 
 
 class Profile(models.Model):
     user         = models.OneToOneField(User, on_delete=models.CASCADE)
-    # photo        = models.
+    # photo        = models.ImageField(default='default_user.jpg', upload_to='profile_pics')
     about_myself = models.TextField(max_length=256, null=True, blank=True)
 
 
 class Tweet(models.Model):
-    user              = models.ForeignKey(User, on_delete=models.CASCADE)
+    author            = models.ForeignKey(User, on_delete=models.CASCADE)
     content           = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     update_datetime   = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('tweet-details', kwargs={'pk': self.pk})
