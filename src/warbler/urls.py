35f080fdd2b7c2808a9cd7f0bf0837fc18b9a626
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-11 23:52:45 +0100

    add paths for login. logout, create_tweet and tweet_details

--- src/warbler/urls.py
+++ src/warbler/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from app_warbler import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.ListAllTweetsView.as_view(), name='list-all-tweets')
+    path('', views.ListAllTweetsView.as_view(), name='list-all-tweets'),
+    path('create_tweet', views.CreateTweetView.as_view(), name='create-tweet'),
+    path('login/', auth_views.LoginView.as_view(template_name='app_warbler/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='app_warbler/logout.html'), name='logout'),
+    path('tweet_details/<int:pk>', views.TweetDetailsView.as_view(), name='tweet-details')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
