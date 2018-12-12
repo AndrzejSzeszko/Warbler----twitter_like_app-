cfaa2d1e8dda3444e7eeadc40a34e5b3515cc4d1
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-12 21:43:13 +0100

    add CreateUserForm

--- src/app_warbler/forms.py
+++ src/app_warbler/forms.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3.7
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
 from . import models
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model  = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 class ProfileForm(forms.ModelForm):
