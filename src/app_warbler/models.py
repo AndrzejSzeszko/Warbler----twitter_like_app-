ea0bc6a558358c0f60d9ec9670e32778b04d816c
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-13 02:15:55 +0100

    add fnc get_sentinel_user and Message model

--- src/app_warbler/models.py
+++ src/app_warbler/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='Deleted user')[0]
+
+
 class Profile(models.Model):
     user         = models.OneToOneField(User, on_delete=models.CASCADE)
     # photo        = models.ImageField(default='default_user.jpg', upload_to='profile_pics')
@@ -18,3 +22,29 @@ class Tweet(models.Model):
 
     def get_absolute_url(self):
         return reverse('tweet-details', kwargs={'pk': self.pk})
+
+
+# class Comment(models.Model):
+#     author = models.ForeignKey(
+#         User,
+#         on_delete=models.SET(get_sentinel_user),
+#         related_name='messages_from_user'
+#     )
+#     content           = models.TextField()
+#     creation_datetime = models.DateTimeField(auto_now_add=True)
+
+
+class Message(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET(get_sentinel_user),
+        related_name='messages_from_user'
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET(get_sentinel_user),
+        related_name='messages_to_user'
+    )
+    content           = models.TextField()
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    is_read           = models.BooleanField(default=False)
