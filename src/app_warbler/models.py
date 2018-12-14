625697bb8b7c6f66d4b3b78a27d91d9ad9ee2a17
AndrzejSzeszko <andrzejgustaw@gmail.com>
2018-12-14 19:42:42 +0100

    overwrite __str__ in all models; add 'is_blocked = models.BooleanField(default=False)' in all models

--- src/app_warbler/models.py
+++ src/app_warbler/models.py
@@ -13,15 +13,22 @@ class Profile(models.Model):
     # photo        = models.ImageField(default='default_user.jpg', upload_to='profile_pics')
     about_myself = models.TextField(max_length=256, null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.user.username}\' profile'
+
 
 class Tweet(models.Model):
     author            = models.ForeignKey(User, on_delete=models.CASCADE)
     tweet_content     = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     update_datetime   = models.DateTimeField(auto_now=True)
+    is_blocked        = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse('tweet-details', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return f'Tweet {self.pk} by {self.author} on {self.update_datetime}'
 
 
 class Comment(models.Model):
@@ -29,6 +36,10 @@ class Comment(models.Model):
     tweet             = models.ForeignKey(Tweet, on_delete=models.CASCADE)
     comment_content   = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
+    is_blocked        = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Comment {self.pk} on tweet {self.tweet_id} by {self.author} on {self.creation_datetime}'
 
 
 class Message(models.Model):
@@ -37,3 +48,7 @@ class Message(models.Model):
     message_content   = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     is_read           = models.BooleanField(default=False)
+    is_blocked        = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.from_user} -> {self.to_user} on {self.creation_datetime}'
