e74e42967b7b7e509109df1b763f6b4981405de4
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-22 20:57:12 +0100

    add UpdateUserForm and UserAndProfileUpdateForm

--- src/app_warbler/forms.py
+++ src/app_warbler/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from . import models
+from betterforms.multiform import MultiModelForm
 
 
 class CreateUserForm(UserCreationForm):
@@ -11,10 +12,23 @@ class CreateUserForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model  = User
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model  = models.Profile
-        fields = '__all__'
+        fields = ['photo', 'about_myself']
+
+
+class UserAndProfileUpdateForm(MultiModelForm):
+    form_classes = {
+        'user': UpdateUserForm,
+        'profile': ProfileForm
+    }
 
 
 class TweetForm(forms.ModelForm):
