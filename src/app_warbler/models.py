0141f4cbb62831693d942e81c80fa42857d119c9
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-01-22 20:56:29 +0100

    add "is_blocked" field in profile model; replace User class calls with get_user_model() calls; add datetime formatting in some of __str__

--- src/app_warbler/models.py
+++ src/app_warbler/models.py
@@ -10,9 +10,10 @@ def get_sentinel_user():
 
 
 class Profile(models.Model):
-    user         = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo        = models.ImageField(default='default_user.jpg', upload_to='profile_pics')
+    user         = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    photo        = models.ImageField(default='default_user.jpg', upload_to='profile_pics', )
     about_myself = models.TextField(max_length=256, null=True, blank=True)
+    is_blocked   = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.user.username}\' profile'
@@ -27,7 +28,7 @@ class Profile(models.Model):
 
 
 class Tweet(models.Model):
-    author            = models.ForeignKey(User, on_delete=models.CASCADE)
+    author            = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     tweet_content     = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     update_datetime   = models.DateTimeField(auto_now=True)
@@ -37,27 +38,27 @@ class Tweet(models.Model):
         return reverse('tweet-details', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return f'Tweet {self.pk} by {self.author} on {self.update_datetime}'
+        return f'Tweet {self.pk} by {self.author} on {self.update_datetime.strftime("%Y/%m/%d %H:%M:%S")}'
 
 
 class Comment(models.Model):
-    author            = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user))
+    author            = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user))
     tweet             = models.ForeignKey(Tweet, on_delete=models.CASCADE)
     comment_content   = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     is_blocked        = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Comment {self.pk} on tweet {self.tweet_id} by {self.author} on {self.creation_datetime}'
+        return f'Comment {self.pk} on tweet {self.tweet_id} by {self.author} on {self.creation_datetime.strftime("%Y/%m/%d %H:%M:%S")}'
 
 
 class Message(models.Model):
-    from_user         = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user), related_name='messages_from_user')
-    to_user           = models.ForeignKey(User, on_delete=models.SET(get_sentinel_user), related_name='messages_to_user')
+    from_user         = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user), related_name='messages_from_user')
+    to_user           = models.ForeignKey(get_user_model(), on_delete=models.SET(get_sentinel_user), related_name='messages_to_user')
     message_content   = models.TextField()
     creation_datetime = models.DateTimeField(auto_now_add=True)
     is_read           = models.BooleanField(default=False)
     is_blocked        = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.from_user} -> {self.to_user} on {self.creation_datetime}'
+        return f'{self.from_user} -> {self.to_user} on {self.creation_datetime.strftime("%Y/%m/%d %H:%M:%S")}'
